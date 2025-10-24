39393b04f1286622e3d7a39213a548c8668aa66f
Kourosh <picorog83@gmail.com>
2025-10-24 18:51:41 +0330

    🔧 Chore: Add config files needed for `liara`

---
+++ main.py
@@ -0,0 +1,8 @@
+"""Module contains entry point for liara development."""
+
+from src.main import app
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8090)
